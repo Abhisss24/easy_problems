6f76937f90300da7a2767aaad6228d45615986ad
Abhisss24 <abhinavr2456@gmail.com>
2025-08-16 22:09:43 +0530

    Create sum_of_given_numbers.cpp

---
+++ sum_of_given_numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+   cout<<"ENTER THE 1ST NUMBER";
+   cin>>a;
+   cout<<"ENTER THE 2ND NUMBER";
+   cin>>b;
+   c=a+b;
+   cout<<"THE SUM OF THWO NUMBERS IS : "<< c;
+
+    return 0;
+}
