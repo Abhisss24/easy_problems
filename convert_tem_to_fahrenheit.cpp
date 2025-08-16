75ec2b828aab084451c929739e19dfe5013a562e
Abhisss24 <abhinavr2456@gmail.com>
2025-08-16 22:33:27 +0530

    Create convert_tem_to_fahrenheit.cpp

---
+++ convert_tem_to_fahrenheit.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a,c;
+    cout<<"ENTER THE TEMPERATURE IN CELSIUS - ";
+    cin >> a ;
+     c=(a*(9.0/5.0))+32;
+   cout<<"THE TEMPERATURE IN FAHRENHEIT IS  - " << c;
+
+    return 0;
+}
