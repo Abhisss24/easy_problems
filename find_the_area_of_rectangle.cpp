0e0e417450eb88e2979dfa193f2a11665b8c7b70
Abhisss24 <abhinavr2456@gmail.com>
2025-08-16 22:22:11 +0530

    Create find_the_area_of_rectangle.cpp

---
+++ find_the_area_of_rectangle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cout<<"ENTER THE LENGHT - ";
+    cin >> a ;
+    cout<<"ENTER THE WIDTH - ";
+    cin>> b ;
+    c=a*b;
+   cout<<"THE AREA OF THE RECTANGLE IS - " << c;
+
+    return 0;
+}
