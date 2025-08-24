81762c4e5b393aa0c449d9b31e8a69089ea533ab
Abhinav R <abhinavr2456@gmail.com>
2025-08-24 14:02:26 +0530

    Create the_given_number_possitive_negative_or_zero.cpp

---
+++ the_given_number_possitive_negative_or_zero.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a;
+    cout<<"ENTER THE NUMBER TO FIND POSITIVE NEGATIVE OR ZERO - ";
+    cin >> a ;
+    if (a > 0){
+    cout<< "THE NUMBER IS POSITIVE";}
+      else if  (a==0){
+    cout<< "THE NUMBER IS ZERO";}
+      else {
+     cout<< "THE NUMBER IS NEGATIVE";}
+    return 0;
+}
