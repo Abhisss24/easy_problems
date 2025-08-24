9a6b942b42793ed2fd6b2c63e8d8863ab5f6a321
Abhinav R <abhinavr2456@gmail.com>
2025-08-24 13:49:24 +0530

    Create find_simple_interest.cpp

---
+++ find_simple_interest.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,p,s;
+    float r;
+    cout<<"ENTER THE PRINCIPLE - ";
+    cin >> p ;
+     cout<<"ENTER THE INTEREST RATE - ";
+    cin>> r;
+  cout<<"ENTER THE TIME IN YEARS - ";
+cin>> t;
+s=p*r*t;
+cout <<"THE SIMPLE INTEREST IS  - " << s;
+
+    return 0;
+}
