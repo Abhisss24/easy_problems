06f637a8a2a731e83ab3df4114237b7343c12601
Abhinav R <abhinavr2456@gmail.com>
2025-08-24 14:08:12 +0530

    Create reminder_when_divided_by_given_numbers.cpp

---
+++ reminder_when_divided_by_given_numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cout<<"ENTER THE 1ST NUMBER - ";
+    cin >> a ;
+    cout<<"ENTER THE 2NDST NUMBER - ";
+    cin >> b ;
+   c= a%b ;
+   cout<<"THE REMINDER WHEN IST NUMBER DIVIDED BY OTHER IS "<< c;
+    return 0;
+}
