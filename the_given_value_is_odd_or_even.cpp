a3680892bfa3effc76e373e985a6a8c896977499
Abhinav R <abhinavr2456@gmail.com>
2025-08-24 14:15:12 +0530

    Create the_given_value_is_odd_or_even.cpp

---
+++ the_given_value_is_odd_or_even.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cout<<"ENTER THE NUMBER TO CHECK ODD OR EVEN - ";
+    cin >> a ;
+    if (a%2==0) {
+   cout<<"THE VALUE ENTERED IS EVEN";}
+    else {
+      cout<<"THE ENTERED VALUE IS ODD ";
+    }
+    return 0;
+}
