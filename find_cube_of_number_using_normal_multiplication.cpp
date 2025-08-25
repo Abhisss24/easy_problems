5967300e9a5d91ef9e836e07aec1ca18f4de3486
Abhinav R <abhinavr2456@gmail.com>
2025-08-25 20:14:27 +0530

    Create find_cube_of_number_using_normal_multiplication.cpp

---
+++ find_cube_of_number_using_normal_multiplication.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float a,b;
+    cout<<"ENTER THE NUMBER TO FIND THE SQUARE ";
+    cin>>a;
+    b=a*a*a;
+    cout<<"THE SQUARE OF "<< a << " NUMBER IS : "<<b; 
+ return 0;
+}
