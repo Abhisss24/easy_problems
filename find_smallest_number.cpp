62f880a408af96885d3b5694ed0310cb9c5fac9a
Abhinav R <abhinavr2456@gmail.com>
2025-08-25 20:04:22 +0530

    Create find_smallest_number.cpp

---
+++ find_smallest_number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a,b;
+    cout<<"ENTER THE 1ST NUMBER";
+    cin>>a;
+    cout<<"ENTER THE 2nd NUMBER";
+    cin>>b;
+    if (a>b)
+    cout<<"THE SMALLEST NUMBER IS : "<<b;
+    else
+     cout<<"THE SMALLEST NUMBER IS : "<<a; 
+ return 0;
+}
