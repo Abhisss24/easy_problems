5f531db9cbcb6c327868859794ca8e5beb72abc7
Abhinav R <abhinavr2456@gmail.com>
2025-08-25 20:11:49 +0530

    Create find_squre_using_pow.cpp

---
+++ find_squre_using_pow.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    float a,b;
+    cout<<"ENTER THE NUMBER TO FIND THE SQUARE ";
+    cin>>a;
+    b=pow(a,2);
+    cout<<"THE SQUARE OF "<< a << " NUMBER IS : "<<b; 
+ return 0;
+}
