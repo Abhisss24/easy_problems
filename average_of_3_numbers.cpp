c0146e30439401bfe716900c39d6ce415cc35dd3
Abhinav R <abhinavr2456@gmail.com>
2025-08-25 19:47:00 +0530

    Create average_of_3_numbers.cpp

---
+++ average_of_3_numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a,b,c,avg;
+    cout<<"ENTER THE 1ST NUMBER";
+    cin>>a;
+    cout<<"ENTER THE 2nd NUMBER";
+    cin>>b;
+    cout<<"ENTER THE 3rd NUMBER";
+    cin>>c;
+    avg=(a+b+c)/3.00;
+    cout<<"THE AVERAGE OF 3 NUMBERS IS :"<<avg;
+ return 0;
+}
