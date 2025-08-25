5a0dcd5f462f823bd5907a887aa9951eef6c37b7
Abhinav R <abhinavr2456@gmail.com>
2025-08-25 19:55:55 +0530

    Create find_largest_number.cpp

---
+++ find_largest_number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a,b;
+    cout<<"ENTER THE 1ST NUMBER";
+    cin>>a;
+    cout<<"ENTER THE 2nd NUMBER";
+    cin>>b;
+    if (a>b)
+    cout<<"THE LARGEST NUMBER IS : "<<a;
+    else
+     cout<<"THE LARGEST NUMBER IS : "<<b; 
+ return 0;
+}
