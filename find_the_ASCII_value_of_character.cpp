681996800d42cc72d792525003d55bc0bb85ce8e
Abhinav R <abhinavr2456@gmail.com>
2025-08-26 19:46:20 +0530

    Create find_the_ASCII_value_of_character.cpp

---
+++ find_the_ASCII_value_of_character.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+   char a;
+   int b;
+    cout<<"ENTER THE BASE  ";
+    cin>>a;
+    b=(int)a;
+    cout<<"THE ASCII OF THE GIVEN CHARACTER IS : "<<b; 
+ return 0;
+}
