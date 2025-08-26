b5b15c95b626594396b89f85eb728a7f655e2976
Abhinav R <abhinavr2456@gmail.com>
2025-08-26 19:09:42 +0530

    Create find_arear_of_circle.cpp

---
+++ find_arear_of_circle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float r,a;
+    cout<<"ENTER THE RADIUS OF CIRCLE ";
+    cin>>r;
+    a=3.14*r*r;
+    cout<<"THE AREA OF CIRCLE IS : "<<a; 
+ return 0;
+}
