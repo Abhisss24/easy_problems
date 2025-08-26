91d26b3d57f62f65fb23ddfb98c37cc75f08f533
Abhinav R <abhinavr2456@gmail.com>
2025-08-26 19:12:11 +0530

    Create find_circumference_of_circle.cpp

---
+++ find_circumference_of_circle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float r,c;
+    cout<<"ENTER THE RADIUS OF CIRCLE ";
+    cin>>r;
+    c=3.14*2*r;
+    cout<<"THE CIRCUMFERENCE OF CIRCLE IS : "<<c; 
+ return 0;
+}
