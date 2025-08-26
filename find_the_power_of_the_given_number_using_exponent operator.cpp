7f2e4fc834ee7933c4a2bc22fddb8f86d1047df1
Abhinav R <abhinavr2456@gmail.com>
2025-08-26 19:23:51 +0530

    Create find_the_power_of_the_given_number_using_exponent operator.cpp

---
+++ find_the_power_of_the_given_number_using_exponent operator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    float b,e,v;
+    cout<<"ENTER THE BASE  ";
+    cin>>b;
+    cout<<"ENTER THE EXPONENT  ";
+    cin>>e;
+    v=pow(b,e);
+    cout<<"THE POWER OF THE GIVEN VALUE IS : "<<v; 
+ return 0;
+}
