c686a7e66f0476cf49979128f896aa1346faddf7
Abhinav R <abhinavr2456@gmail.com>
2025-08-29 00:02:51 +0530

    Create product_of_first_10_natural_numbers.cpp

---
+++ product_of_first_10_natural_numbers.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a,i;
+    a=1;
+    for (i=1;i<=10;i++){
+        a=a*i;
+    }
+    cout<<"PRODUCT OF 1ST 10 NATURAL NUMBERS IS : "<<a;
+}
