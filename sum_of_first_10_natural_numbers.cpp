50c075f5227fb379e76df341725faf02fa4cbae7
Abhinav R <abhinavr2456@gmail.com>
2025-08-29 00:00:26 +0530

    Create sum_of_first_10_natural_numbers.cpp

---
+++ sum_of_first_10_natural_numbers.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a,i;
+    a=0;
+    for (i=1;i<=10;i++){
+        a=a+i;
+    }
+    cout<<"SUM OF 1ST 10 NATURAL NUMBERS IS : "<<a;
+}
