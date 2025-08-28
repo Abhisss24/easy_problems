28fd6a77b60b909d17a2dd369244006dcf7657f1
Abhinav R <abhinavr2456@gmail.com>
2025-08-29 00:15:07 +0530

    Create find_character_vowelor_consonant.cpp

---
+++ find_character_vowelor_consonant.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main() {
+    char x;
+   cout<<"ENTER THE CHARACTER TO CHECK VOWEL OR CONSONANT : ";
+   cin>>x;
+   if (x=='a'){
+    cout<<"CHARACTER IS VOWEL";
+   }
+   else if (x=='e'){
+    cout<<"CHARACTER IS VOWEL";
+   }
+   else if (x=='i'){
+    cout<<"CHARACTER IS VOWEL";
+   }
+   else if (x=='o'){
+    cout<<"CHARACTER IS VOWEL";
+   }
+   else if (x=='u'){
+    cout<<"CHARACTER IS VOWEL";
+   } 
+   else {
+    cout<<"THE CHARACTER IS CONSONANT";
+   }
+}
